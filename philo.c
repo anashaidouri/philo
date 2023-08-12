078ee038d602b0791e40ad4841a609abaec93938
Anas Haidouri <ahaidour@e1r3p12.1337.ma>
2023-08-12 14:46:23 +0100

    ds

--- philo.c
+++ philo.c
@@ -6,13 +6,13 @@
 /*   By: ahaidour <ahaidour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 15:22:08 by ahaidour          #+#    #+#             */
-/*   Updated: 2023/08/10 18:35:33 by ahaidour         ###   ########.fr       */
+/*   Updated: 2023/08/12 13:54:53 by ahaidour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	informations(t_info *info, int ac, char **av)
+int	informations(t_info *args, int ac, char **av)
 {
 	if (ft_atoi(av[1]) > INT_MAX || ft_atoi(av[2]) > INT_MAX
 		|| ft_atoi(av[3]) > INT_MAX || ft_atoi(av[4]) > INT_MAX)
@@ -20,14 +20,14 @@ int	informations(t_info *info, int ac, char **av)
 	if (ft_atoi(av[1]) < 0 || ft_atoi(av[2]) < 0
 		|| ft_atoi(av[3]) < 0 || ft_atoi(av[4]) < 0)
 		return (0);
-	info->nb_philo = ft_atoi(av[1]);
-	info->time_to_die = ft_atoi(av[2]);
-	info->time_to_eat = ft_atoi(av[3]);
-	info->time_to_sleep = ft_atoi(av[4]);
+	args->nb_philo = ft_atoi(av[1]);
+	args->time_to_die = ft_atoi(av[2]);
+	args->time_to_eat = ft_atoi(av[3]);
+	args->time_to_sleep = ft_atoi(av[4]);
 	if (ac == 6)
 	{
-		info->is_given_max = 1;
-		info->max_eaten = ft_atoi(av[5]);
+		args->is_given_max = 1;
+		args->max_eaten = ft_atoi(av[5]);
 		if (ft_atoi(av[5]) > INT_MAX || ft_atoi(av[5]) < 0)
 			return (0);
 	}
@@ -35,20 +35,22 @@ int	informations(t_info *info, int ac, char **av)
 	return (1);
 }
 
-long get_current_time_milliseconds(void) {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+long	get_time_in_millisecond(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
-void initialize_mutexes(t_info *infos) {
-    pthread_mutex_init(&infos->time_mutex, NULL);
-    pthread_mutex_init(&infos->print_mutex, NULL);
-    pthread_mutex_init(&infos->read_mutex, NULL);
-    pthread_mutex_init(&infos->increment_mutex, NULL);
+void initialize_mutexes(t_data *data, t_info *args) {
+    pthread_mutex_init(&data->time_mutex, NULL);
+    pthread_mutex_init(&data->print_mutex, NULL);
+    pthread_mutex_init(&data->read_mutex, NULL);
+    pthread_mutex_init(&data->increment_mutex, NULL);
     int i = 0;
-    while (i < infos->nb_philo) {
-        if (pthread_mutex_init(&infos->forks[i], NULL) != 0) {
+    while (i < args->nb_philo) {
+        if (pthread_mutex_init(&data->forks[i], NULL) != 0) {
             retour_erreur("Failed to initialize mutex");
             // Handle error as needed
         }
@@ -56,37 +58,108 @@ void initialize_mutexes(t_info *infos) {
     }
 }
 
-void destroy_mutexes(t_info *infos) {
+void destroy_mutexes(t_data *data, t_info *args) {
     int i = 0;
-    while (i < infos->nb_philo) {
-        pthread_mutex_destroy(&infos->forks[i]);
+    while (i < args->nb_philo) {
+        pthread_mutex_destroy(&data->forks[i]);
         // Handle any potential error, if necessary
         i++;
     }
-    pthread_mutex_destroy(&infos->print_mutex);
-    pthread_mutex_destroy(&infos->time_mutex);
-    pthread_mutex_destroy(&infos->read_mutex);
-    pthread_mutex_destroy(&infos->increment_mutex);
+    pthread_mutex_destroy(&data->print_mutex);
+    pthread_mutex_destroy(&data->time_mutex);
+    pthread_mutex_destroy(&data->read_mutex);
+    pthread_mutex_destroy(&data->increment_mutex);
 }
 
-void	create_philos(t_info *infos)
+void	ft_sleep(int time_to_eat)
 {
-	infos->init_time = get_current_time();
-	initializing_infos(infos);
+	long	time;
+
+	time = get_time_in_millisecond();
+	while (get_time_in_millisecond() - time < time_to_eat)
+		usleep(100);
+}
+
+// void	print_message(t_philo *philo, char *str, int i)
+// {
+// 	pthread_mutex_lock(&philo->infos->print_mutex);
+// 	printf("%ld %d %s\n", get_current_time() - philo->infos->init_time,
+// 		philo->id + 1, str);
+// 		// printf("********* %d\n", i);
+// 	// if ( !i )
+// 		// exit ( 0 );
+// 	if (i)
+// 	{
+// 		pthread_mutex_unlock(&philo->infos->print_mutex);
+// 	}
+// }
+
+void	*routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->id % 2)
+		usleep(200);
+	while (1)
+	{
+		// eating(philo);
+		// print_message(philo, "is sleeping", 1);
+		ft_sleep(philo->args->time_to_sleep);
+		// print_message(philo, "is thinking", 1);
+	}
+}
+
+void	creating_threads(t_data *data, t_info *args, int i)
+{
+	while (i < args->nb_philo)
+	{
+		data->philos[i].id = i;
+		data->philos[i].eaten_meals = 0;
+		data->philos[i].right_fork = i;
+		data->philos[i].args = args;
+		data->philos[i].last_eat_time = data->init_time;
+		if (i == args->nb_philo - 1)
+			data->philos[i].left_fork = data->philos[0].right_fork;
+		else
+			data->philos[i].left_fork = i + 1;
+		if (pthread_create(&data->philos[i].thread, NULL, &routine,
+				&data->philos[i]) != 0)
+		{
+			retour_erreur("creating thread failed");
+			return ;
+		}
+		i++;
+	}
+}
+
+void	initializing_infos(t_data *data,t_info *args)
+{
+	int	i;
+
+	i = 0;
+	creating_threads(data, args, i);
+	while (1)
+		if (check_death(args, i))
+			return ;
 }
 
 int	main(int ac, char **av)
 {
-	t_info infos;
+	t_info args;
+	t_data data;
 	if (ac == 5 || ac == 6)
 	{
 		arg_check(ac, av);
-		if (informations(&infos, ac, av))
+		if (informations(&args, ac, av))
 		{
-			initialize_mutexes(&infos);
-			// create_philos(&infos);
+			data.forks = malloc(sizeof(pthread_mutex_t) * args.nb_philo);
+			data.philos = malloc(sizeof(t_philo) * args.nb_philo);
+			initialize_mutexes(&data, &args);
+			data.init_time = get_time_in_millisecond();
+			initializing_infos(&data, &args);
 			sleep(1);
-			destroy_mutexes(&infos);
+			destroy_mutexes(&data, &args);
 		}
 		else
 			retour_erreur("Error");
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: ahaidour <ahaidour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 15:22:28 by ahaidour          #+#    #+#             */
-/*   Updated: 2023/08/10 16:55:41 by ahaidour         ###   ########.fr       */
+/*   Updated: 2023/08/12 13:19:10 by ahaidour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ typedef struct t_philo
 	int					left_fork;
 	long				last_eat_time;
 	int					eaten_meals;
-	struct s_args		*infos;
+	struct t_info		*args;
+	struct t_data		*data;
 }						t_philo;
 
 typedef struct t_info
@@ -41,13 +42,18 @@ typedef struct t_info
 	int time_to_sleep;
 	int is_given_max;
 	int max_eaten;
+}			t_info;
+
+typedef struct t_data
+{
+	long			init_time;
 	pthread_mutex_t time_mutex;
     pthread_mutex_t print_mutex;
     pthread_mutex_t read_mutex;
     pthread_mutex_t increment_mutex;
     pthread_mutex_t *forks;
 	struct t_philo	*philos;
-}			t_info;
+}					t_data;
 
 void 		retour_erreur(char *str);
 void		arg_check(int ac, char **av);
